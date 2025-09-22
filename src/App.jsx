35b5bc44c08a78facf5e8eaf9ca495f30a6292b8
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-09-22 23:36:49 +0530

    added timer which works on hot reload but all animations are lost on refreshing the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import { useState,useRef,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {animate,createScope,createSpring,createDraggable} from "animejs";
+import {animate, createScope, createSpring, createDraggable, utils} from "animejs";
+import {createTimer} from 'animejs';
 
+function Timer(){
+    const [$time,$count] = utils.$('.value');
+    createTimer({
+        duration: 1000,
+        loop: true,
+        frameRate: 30,
+        onUpdate: self => $time.innerHTML = self.currentTime,
+        onLoop: self => $count.innerHTML = self._currentIteration
+    });
+    return (
+        <div className="large centered row">
+            <div className="half col">
+                <pre className="large log row">
+                    <span className="label">current time</span>
+                    <span className="value lcd">0</span>
+                </pre>
+            </div>
+            <div className="half col">
+                <pre className="large log row">
+                    <span className="label">callback fired</span>
+                    <span className="value lcd">0</span>
+                </pre>
+            </div>
+        </div>
+    )
+}
 function App() {
     const vite = useRef(null);
     const scope=useRef(null);
-    const reactanim = useRef(null);
     const [rotation, setRotation] = useState(0);
     useEffect(()=>{
         scope.current=createScope({vite}).add(self=>{
@@ -21,6 +47,7 @@ function App() {
         });
         return ()=>scope.current.revert();
     },[])
+    const reactanim = useRef(null);
     useEffect(()=>{
         scope.current=createScope({reactanim}).add(self=>{
             createDraggable('.react',{
@@ -34,32 +61,33 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-          <div ref={vite} className="size-bounce">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo vite" alt="Vite logo" />
-        </a>
+      <>
+          <div>
+              <div ref={vite} className="size-bounce">
+                  <a href="https://vite.dev" target="_blank">
+                      <img src={viteLogo} className="logo vite" alt="Vite logo"/>
+                  </a>
+              </div>
+              <div ref={reactanim} className="dragable">
+                  <a href="https://react.dev" target="_blank">
+                      <img src={reactLogo} className="logo react" alt="React logo"/>
+                  </a>
+              </div>
           </div>
-          <div ref={reactanim} className="dragable">
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <h1>Vite + React</h1>
+          <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+              <p>
+                  Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
           </div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+          </p>
+          <Timer/>
+      </>
   )
 }
 
