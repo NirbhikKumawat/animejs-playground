3c5cb15bbc2322d77fdb651d5b60901be54e25c5
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-09-22 23:01:22 +0530

    Always add ref in div.Added sizebouncespring to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
+import { useState,useRef,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {animate,createScope,createSpring,createDraggable} from "animejs";
 
 function App() {
+    const root = useRef(null);
+    const scope=useRef(null);
+    const [rotation, setRotation] = useState(0);
+    useEffect(()=>{
+        scope.current=createScope({root}).add(self=>{
+            animate('.logo',{
+                scale:[
+                    {to:1.25,ease: 'inOut(3)',duration:500},
+                    {to:1,ease:createSpring({stiffness:300})}
+                ],
+                loop:true,
+            });
+        });
+        return ()=>scope.current.revert();
+    },[])
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+          <div ref={root} className="size-bounce">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+          </div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
